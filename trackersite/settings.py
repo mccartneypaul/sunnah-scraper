bfce2e74caf616bc4c6c4144e419f57ba7fbadee
Paul McCartney <paulmc@1791.com>
2026-05-02 16:41:09 -0400

    feat: add ingestion app from init

--- trackersite/settings.py
+++ trackersite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'corpus.apps.CorpusConfig',
+    'ingestion.apps.IngestionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
